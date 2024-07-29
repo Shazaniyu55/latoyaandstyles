c0dc8a5a87c4208560a5913d6a4236cd6cd50166
compunet <compunet@compunets-MacBook-Pro.local>
2024-07-29 09:32:04 +0100

    waliyu2@#!

--- server.js
+++ server.js
@@ -196,7 +196,7 @@ app.post('/admin-login', async (req, res) => {
 
 // Middleware to authenticate JWT tokens
 const authMiddleware = (req, res, next) => {
-    const token = req.headers['authorization']?.split(' ')[1];
+    const token = req.cookies.token;
     if (!token) {
         return res.status(401).json({ message: 'No token provided' });
     }
