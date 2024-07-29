f398b66bd59840f1e517a8a43554fd3f84ddb215
compunet <compunet@compunets-MacBook-Pro.local>
2024-07-29 07:49:48 +0100

    waliyu

--- model/product.model.js
+++ model/product.model.js
@@ -22,4 +22,5 @@ const ProductSchema = new mongoose.Schema({
   }
 });
 
-module.exports = mongoose.model('Product', ProductSchema);
+const Product = mongoose.model('Product', ProductSchema);
+module.exports = Product
