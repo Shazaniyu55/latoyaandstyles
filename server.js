30dc87ad171a2f5ba45748fd12a128eb8b5f394f
compunet <compunet@compunets-MacBook-Pro.local>
2024-07-29 10:20:11 +0100

    waliyu2@#!

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.use(express.static(path.join(__dirname, 'assets')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); 
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: true}))
 app.use('/api/auth', authRoutes)
 app.use('/api/product', productRoutes);
 app.get('/',  async(req, res) => {
